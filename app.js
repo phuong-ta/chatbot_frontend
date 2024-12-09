4cca5475ae40cebdaa0417008d30eece459bbe89
Phuong Ta <phuongta@metropolia.fi>
2024-12-09 12:14:55 +0200

    add view

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ const express = require('express');
 // Next initialize the application
 const app = express();
 
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+
 // routing path
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+  // The render method takes the name of the html
+  // page to be rendered as input.
+  // This page should be in views folder 
+  // in the root directory.
+  let data = {
+      name: 'Akashdeep',
+      hobbies: ['playing football', 'playing chess', 'cycling']
+  }
+
+  res.render('home', { data: data });
 });
+
+
+
 
 // Start the server
 app.listen(3000, () => {

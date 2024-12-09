a9b6ffa18a2e6714e929844564d7def8b3bbd7d2
Phuong Ta <phuongta@metropolia.fi>
2024-12-09 18:13:54 +0200

    test with api

--- app.js
+++ app.js
@@ -8,20 +8,25 @@ const app = express();
 app.set('view engine', 'ejs');
 
 
-// routing path
-app.get('/', (req, res) => {
 
-  // The render method takes the name of the html
-  // page to be rendered as input.
-  // This page should be in views folder 
-  // in the root directory.
-  let data = {
-      name: 'Akashdeep',
-      hobbies: ['playing football', 'playing chess', 'cycling']
-  }
-
-  res.render('index', { data: data });
+// Route to call the API and render the data 
+app.get('/', async (req, res) => { 
+  try { 
+    // Create the URL with the provided date 
+    const backend_url ="https://chatbot-backend-260287853257.europe-north1.run.app/list_files"
+    // Call the API with GET method 
+    const response = await axios.get(backend_url); 
+    // Extract the EUR_per_kWh values into a list 
+    const prices = response.data.map(item => item.EUR_per_kWh); 
+    // Pass data into the render method 
+    res.render('prices', { prices }); 
+  } catch (error) { 
+    console.error('Error fetching data from API:', error); 
+    res.status(500).send('Error fetching data from API'); 
+  } 
 });
+
+
 
 
 
